f361ebfbed9f29660de6cd852fbdb89c3e579333
obayna <obayna92@gmail.com>
2026-01-03 05:05:12 +0200

    update4

--- Frontend/src/pages/Signup.js
+++ Frontend/src/pages/Signup.js
@@ -19,7 +19,7 @@ const Signup = () => {
       return;
     }
 
-    axios.post('https://petadopation-production.up.railway.app/signup', { 
+    axios.post('https://petadopation-production.up.railway.app/api/signup', { 
         username: name, 
         email: email, 
         password: password 
